ec87d702b00dd23cbee15e9a0560e9d7ad81b28c
Barry Kooij <barrykooij@users.noreply.github.com>
2023-06-29 09:41:30 +0200

    Added priority and accepted_args properties to Events

--- src/Events/Event.php
+++ src/Events/Event.php
@@ -10,17 +10,27 @@ abstract class Event {
 	private $name;
 	private $action;
 	private $endpoint;
+	private $priority = 10;
+	private $accepted_args = 1;
 
-	public function __construct( string $name, string $action, $endpoint ) {
-		$this->name     = $name;
-		$this->action   = $action;
-		$this->endpoint = $endpoint;
+	public function __construct(
+		string $name,
+		string $action,
+		string $endpoint,
+		int $priority = 10,
+		int $accepted_args = 1
+	) {
+		$this->name          = $name;
+		$this->action        = $action;
+		$this->endpoint      = $endpoint;
+		$this->priority      = $priority;
+		$this->accepted_args = $accepted_args;
 
 		$this->add_listener();
 	}
 
 	private function add_listener() {
-		add_action( $this->action, [ $this, 'listener_callback' ] );
+		add_action( $this->action, [ $this, 'listener_callback' ], $this->priority, $this->accepted_args );
 	}
 
 	public function listener_callback( ...$args ) {
