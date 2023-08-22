dfae41857fa6880d84a1092f8095b61339b53a3c
Barry Kooij <barrykooij@users.noreply.github.com>
2023-08-22 11:37:27 +0200

    Update Event.php

--- src/Events/Event.php
+++ src/Events/Event.php
@@ -7,7 +7,7 @@ use Scanfully\API\EventRequest;
 
 abstract class Event {
 
-	private $event;
+	private $type;
 	private $action;
 	private $priority = 10;
 	private $accepted_args = 1;
@@ -18,7 +18,7 @@ abstract class Event {
 		int $priority = 10,
 		int $accepted_args = 1
 	) {
-		$this->event         = $event;
+		$this->type          = $event;
 		$this->action        = $action;
 		$this->priority      = $priority;
 		$this->accepted_args = $accepted_args;
@@ -30,9 +30,22 @@ abstract class Event {
 		add_action( $this->action, [ $this, 'listener_callback' ], $this->priority, $this->accepted_args );
 	}
 
+	private function get_user(): array {
+		$current_user = wp_get_current_user();
+
+		return [
+			'id'   => $current_user->ID,
+			'name' => $current_user->display_name,
+		];
+	}
+
 	public function listener_callback( ...$args ) {
 		$request = new EventRequest();
-		$request->send_event( [ "event" => $this->event, "data" => $this->get_post_body( $args ) ] );
+		$request->send_event( [
+			"type" => $this->type,
+			"user" => $this->get_user(),
+			"data" => $this->get_post_body( $args )
+		] );
 	}
 
 	abstract protected function get_post_body( array $data ): array;
