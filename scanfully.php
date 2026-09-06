9d3a2dba1578e68f37292f4d9fb9dcac2324defa
Barry Kooij <barrykooij@users.noreply.github.com>
2026-09-06 23:22:33 +0200

    version bump

--- scanfully.php
+++ scanfully.php
@@ -8,7 +8,7 @@
  *
  * @wordpress-plugin
  * Plugin Name: Scanfully
- * Version:     1.7.0-alpha5
+ * Version:     1.7.0-alpha6
  * Plugin URI:  https://scanfully.com/wp-plugin
  * Description: Scanfully is your favorite WordPress performance and health monitoring tool.
  * Author:      Scanfully
